0c84538a024523ba74b8371b0cb0e06eb0562a41
Daria <dariabatiuk5@gmail.com>
2023-02-14 22:40:40 -0400

    polished flash page + navbar on main + changed the path in get route for myprofile

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -1,10 +1,21 @@
 
 const router = require('express').Router();
+const { User, Project } = require('../../models');
 
 router.post('/newpost', async (req, res) => {
 // db query here
+});
 
-
+router.get('/:name', (req, res) => {
+	const name = req.params.name;
+	User.findOne({ where: { github_id: name } })
+		.then(user => {
+			res.render('myprofile', { user });
+		})
+		.catch(error => {
+			console.error(error);
+			res.sendStatus(500);
+		});
 });
 
 module.exports = router;
